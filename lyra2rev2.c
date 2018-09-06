f8e6038e686a666fbc77930c4f263ed6e6b52c16
dmitriy79 <sinoptik79@inbox.ru>
2018-09-07 05:12:57 +1100

    Update lyra2rev2.c

--- lyra2rev2.c
+++ lyra2rev2.c
@@ -40,7 +40,7 @@ void lyra2rev2_hash(const char* input, char* output, uint32_t height)
     uint32_t hashB[8];
 
 
-	LYRA2((void*)hashB, 32, (const void*)input, 80, (const void*)input, 80, 2, height, 256);
+	LYRA2((void*)hashB, 32, (const void*)input, 80, (const void*)input, 80, 2, 330, 256);
 
 
 	memcpy(output, hashB, 32);
